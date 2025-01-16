3a7cc7fcafc14a5a59d283dcbfbc31f2afdbbe4b
Yair-8 <yanakryvokhyzha2014@gmail.com>
2025-01-16 01:03:27 -0500

    the same

--- scripts/components/RequestManager.js
+++ scripts/components/RequestManager.js
@@ -1,7 +1,7 @@
 class RequestManager {
   // Базовий URL для API запитів
   //   static apiBase = "http://localhost:3000/api/v1";
-  static apiBase = "https://https://purses-backend.onrender.com/api/v1";
+  static apiBase = "https://purses-backend.onrender.com/api/v1";
 
   // Метод для отримання повного маршруту до сервера
   static getServerRoute(path) {
